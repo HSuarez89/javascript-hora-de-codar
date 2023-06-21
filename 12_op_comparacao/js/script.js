a5342c85c0e6614f4610d0ec0b6649d6984898a8
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-06-21 15:43:40 -0300

    Curso javascript

---
+++ 12_op_comparacao/js/script.js
@@ -0,0 +1,24 @@
+var idade = 15
+var possuiCarro = 1 //bolean
+
+if(idade >= 18){
+    console.log('Usuário pode tirar habilitação')
+}
+
+if(idade <= 17){
+    console.log("Usuário não pode tirar habilitação")
+}
+
+if(possuiCarro){
+    console.log('O usuário possui um carro.')
+}
+
+var nome = 'Henrique'
+
+if(nome == 'Henrique'){
+    console.log('O seu nome é Henrique')
+}
+
+if(nome != 'Hugo'){
+    console.log('O seu nome não é Hugo')
+}
