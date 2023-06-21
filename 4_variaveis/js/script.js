119cd6855e471e9e5059b7724289cbad6f4acb12
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-06-21 11:21:55 -0300

    Curso javascript

---
+++ 4_variaveis/js/script.js
@@ -0,0 +1,40 @@
+var teste = 1
+console.log(teste)
+
+teste = 'Matheus'
+console.log(teste)
+
+var nome = 'João'
+
+console.log(nome)
+
+//Únicos caracteres especiais
+var $nome = 'ASD'
+var _nome = 'ASD2'
+
+console.log($nome)
+console.log(_nome)
+
+//Números não podem ser o primeiro caractere
+var nome5 = 'ASD3'
+
+console.log(nome5)
+
+//Sintaxe camelCase
+var meuPrimeiroNome = 'Henrique'
+
+console.log(meuPrimeiroNome)
+
+let testando = 1
+const ola = 2
+
+console.log(testando)
+console.log(ola)
+
+var meuNome //undefined
+
+console.log(meuNome)
+
+meuNome = 'Henrique'
+
+console.log(meuNome)
