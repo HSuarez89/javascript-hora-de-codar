4c4d9a3c99f31155e6d15ee525a6d69dfc055722
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-06-21 15:04:17 -0300

    Curso javascript

---
+++ 9_objetos/js/script.js
@@ -0,0 +1,15 @@
+var obj = {
+    nome: 'Henrique',
+    idade: 33,
+    profissao: 'Programador',
+    trabalhando: true
+}
+console.log(obj)
+console.log(typeof obj)
+
+console.log(obj.nome)
+console.log('Minha idade é: ' + obj.idade)
+
+obj.graduacao = true //Criar propriedades dentro do objeto
+
+console.log(obj)
