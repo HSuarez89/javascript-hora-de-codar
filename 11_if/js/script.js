24e4506260e99cb6a8c411e2d1e89d43259fefbe
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-06-21 15:31:02 -0300

    Curso javascript

---
+++ 11_if/js/script.js
@@ -0,0 +1,14 @@
+console.log('Antes do if')
+
+if(true){
+    console.log("Testando if")
+}
+
+console.log('Depois do if')
+
+var idade = 15
+if(idade >= 18){
+    console.log('Pode tirar habilitação')
+}else{
+    console.log('Não pode tirar habilitação ainda')
+}
