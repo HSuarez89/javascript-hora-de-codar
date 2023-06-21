772b7e66e0bb34202559f803198eaed15fae642a
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-06-21 14:30:56 -0300

    Curso javascript

---
+++ 6_string/js/script.js
@@ -0,0 +1,19 @@
+var nome = 'Henrique'
+var sobrenome = 'Suarez'
+console.log(nome)
+console.log(typeof nome)
+
+var nomeCompleto = nome + " " + sobrenome
+
+console.log(typeof "asd")
+
+console.log(nomeCompleto)
+console.log(typeof nomeCompleto)
+
+var frase = 'Essa é uma frase complexa'
+
+console.log(frase)
+console.log(typeof frase)
+
+document.write('Olá, ')
+document.write('Meu nome é: ' + nomeCompleto)
