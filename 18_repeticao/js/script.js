e1ecbd5d085fd2294c340e9b2745d0e1775719d1
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-06-22 10:37:44 -0300

    curso javascript

---
+++ 18_repeticao/js/script.js
@@ -0,0 +1,26 @@
+var numero = 0
+
+while(numero <= 5){
+    console.log(numero)
+    numero ++ // incrementador
+}
+
+//--------------------------------------------
+
+var arr = ['Henrique', 'Hugo', 'Jessica', 'Mariana', '587', 4]
+var y = 0
+
+while(y <= 5){
+    console.log(arr[y])
+    y++
+}
+
+//--------------------------------------------
+
+var palavra = "Henrique"
+var i = 0
+
+while(i <= 7){
+    console.log(palavra[i])
+    i++
+}
