3efe72077b2d10359d6b137e84dc82d5ec1eebc6
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-06-22 09:25:11 -0300

    Curso javascript

---
+++ 13_else_if_e_else/js/script.js
@@ -0,0 +1,21 @@
+var nome = 'Henrique'
+
+if(nome == 'Pedro'){
+    console.log("O nome dele é Pedro")
+}else if(nome == "Matheus"){
+    console.log("O nome dele é Matheus")
+}else if(nome == "Xavier"){
+    console.log("O nome dele é Xavier")
+}else{
+    console.log("Ele possui outro nome")
+}
+
+var idade = 17
+
+if(idade > 20){
+    console.log("Pode entrar na festa.")
+} else if(idade >= 18){
+    console.log("Precisa de autorização.")
+}else {
+    console.log("Não pode entrar na festa.")
+}
