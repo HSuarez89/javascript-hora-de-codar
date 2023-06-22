acaca902ba3ff0bfa4a9790fc0cc2f033dbe2cc0
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-06-22 11:10:28 -0300

    curso javascript

---
+++ 19_for/js/script.js
@@ -0,0 +1,13 @@
+for(var i = 0; i <= 10; i++){
+    console.log('Repetindo o processo: ' + i)
+}
+
+
+var arr = "Henrique" 
+for(var j = 0; j < arr.length; j++){
+    console.log(arr[j])
+}
+
+for(var x = 1; x <= 200; x *= 2){
+    console.log(x)
+}
