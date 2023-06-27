5d4256938bdc1675e758f0f9fbe7312000234c10
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-06-27 14:09:30 -0300

    curso javascript

---
+++ 34_querry_selector/js/script.js
@@ -0,0 +1,22 @@
+var itensClasse = document.getElementsByClassName('item')
+console.log(itensClasse)
+
+//querrySelectorAll
+var itensQuery = document.querySelectorAll("#lista2 li")
+console.log(itensQuery)
+
+var itensQuery2 = document.querySelectorAll("#lista li")
+console.log(itensQuery2)
+
+//querySelector (apenas o primeiro elemento)
+var lista = document.querySelector('#lista')
+console.log(lista)
+
+var primeiraLista = document.querySelector('ul')
+console.log(primeiraLista)
+
+var span = document.querySelector('#paragrafo span')
+console.log(span)
+
+var span = document.querySelectorAll('#paragrafo span')
+console.log(span)
