21b59450680b949f3405028ac95937257413fee0
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-06-27 14:18:40 -0300

    curso javascript

---
+++ 35_alterando_conteudo/js/script.js
@@ -0,0 +1,11 @@
+// selecionar o elemento
+
+var title = document.querySelector('#title')
+
+//innerHTML
+title.innerHTML = "Testando o texto alterado!"
+
+//textContent -> mais recomendado
+
+var subtitle = document.querySelector('.subtitle')
+subtitle.textContent = "Testando o textContent!"
