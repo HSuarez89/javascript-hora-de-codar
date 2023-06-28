363b7766f6b1673f628925b482ccf835efb07b4c
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-06-28 15:29:45 -0300

    curso javascript

---
+++ 46_evento_onload/js/script.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+    console.log('Carregou do DOM')
+    var title2 = document.querySelector('#title')
+    console.log(title2)
+}
+
+console.log("Carregou o JS")
+var title = document.querySelector('#title')
+console.log(title)
