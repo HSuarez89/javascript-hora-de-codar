a9149a1e6cfda9f88babd497d4945d26a32073df
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-06-28 15:00:32 -0300

    curso javascript

---
+++ 44_settimeout_setinterval/js/script.js
@@ -0,0 +1,19 @@
+// setTimeout
+console.log('Antes do timer')
+
+setTimeout(function(){
+
+    console.log('Testando o setTimeout')
+
+}, 2000)
+
+console.log('Depois do timer')
+
+
+// setInterval
+
+setInterval(function(){
+
+    console.log("Testando setInterval")
+
+}, 1000)
