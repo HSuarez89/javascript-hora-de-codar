5f3c193d894c18bd93c7205b441062de4a0227ab
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-06-28 09:49:28 -0300

    Update script.js

--- 36_criando_elementos/js/script.js
+++ 36_criando_elementos/js/script.js
@@ -21,3 +21,11 @@ var titulo3 = document.createElement('h3') //crio a nova tag
 titulo3.appendChild(document.createTextNode('Título 3'))//crio o texto dentro da nova tag
 
 novoTitulo.appendChild(titulo3)//coloco a nova tag com o texto no lugar localizado
+
+// criando header (por minha conta)
+var cabecalho = document.createElement('header') //cria elemento header
+body.appendChild(cabecalho) //coloca o header dentro do body
+var tituloCabecalho = document.createElement('h1') //cria o h1
+tituloCabecalho.appendChild(document.createTextNode('Título no Cabeçalho')) //escreve o texto dentro do h1
+
+cabecalho.appendChild(tituloCabecalho) // coloca o h1 dentro do header que está dentro do body
