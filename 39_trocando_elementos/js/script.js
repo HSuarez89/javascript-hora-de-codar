c63eb3a27ee9ef50de54a2b766635df42946eb82
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-06-28 13:55:47 -0300

    Update script.js

--- 39_trocando_elementos/js/script.js
+++ 39_trocando_elementos/js/script.js
@@ -17,3 +17,20 @@
  //trocar os elementos
 
  pai.replaceChild(el, title) //recebe primeiro o elemento que entra e depois o elemento que sai
+
+
+ //exercício por minha conta
+ //adicionar o item8 novamente na lista e substituí-lo pelo item9
+
+ var novoItem = document.createElement('li')//criando novo elemento da lista
+ novoItem.classList = 'item-exercicio'//atribuição de uma classe para esse novo elemento
+
+ novoItem.appendChild(document.createTextNode('Item8'))//inserção do texto do elemento criado
+ var lista = document.querySelector('body #lista2')//localização do elemento pai do elemento criado
+
+ lista.appendChild(novoItem)//inserção do elemento no elemento pai
+
+ var itemCorreto = document.createElement('li')//criação de mais um elemento
+ itemCorreto.appendChild(document.createTextNode('Item9'))//inserção do texto no novo elemento
+
+ lista.replaceChild(itemCorreto, novoItem)//substituição do primeiro elemento criado pelo segundo
