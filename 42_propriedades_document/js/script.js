e98bf32867b917e3d1ef293423c60aab6de260e6
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-06-28 14:36:02 -0300

    curso javascript

---
+++ 42_propriedades_document/js/script.js
@@ -0,0 +1,14 @@
+//propriedades document
+console.log(document.body)
+console.log(document.head)
+
+console.log(document.links)
+console.log(document.links[1])
+
+document.links[1].textContent = 'Instagram'
+
+console.log(document.URL)
+
+console.log(document.title)
+document.title = '42 - Propriedades do document ;)'
+console.log(document.title)
