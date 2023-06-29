d049c93e249f0f413950da5a440eacba9ed9c1dc
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-06-29 15:24:24 -0300

    curso javascript

---
+++ projeto_final/js/script.js
@@ -0,0 +1,8 @@
+var botao = document.querySelector('#add-btn')
+botao.addEventListener('click', function(){
+    var input = document.querySelector('#task-title')
+    var tarefa = input.value
+    console.log(tarefa)
+})
+
+
