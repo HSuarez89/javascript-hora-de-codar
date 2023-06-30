40664f6afb0564d291105e89e80021681f0e7387
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-06-30 12:02:24 -0300

    atualizando projeto

--- projeto_final/js/script.js
+++ projeto_final/js/script.js
@@ -1,8 +1,26 @@
-var botao = document.querySelector('#add-btn')
-botao.addEventListener('click', function(){
-    var input = document.querySelector('#task-title')
-    var tarefa = input.value
-    console.log(tarefa)
+//adicionando tarefa
+function adicionaTarefa(){
+    tarefa = document.querySelector('#task-title').value
+    if(tarefa){
+        criarTarefa(tarefa)
+    }
+}
+
+//criando a tarefa
+function criarTarefa(){
+    var lista = document.querySelector('#task-list') //localizo a lista ul
+    var elemento = document.querySelector('.template').innerHTML //copio o elemento li que será o template
+    var elementoLista = document.createElement('li') // crio o card que receberá a tarefa
+    elementoLista.innerHTML = elemento
+    lista.appendChild(elementoLista)
+}
+
+
+
+const botao = document.querySelector('#add-btn')
+botao.addEventListener('click', function(e){
+   e.preventDefault()
+   adicionaTarefa()
 })
 
 
